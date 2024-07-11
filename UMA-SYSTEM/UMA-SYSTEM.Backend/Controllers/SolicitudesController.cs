94141a6e76440bb15df2d95f37e881b2d6ec4a39
Gerardo Lanza <glanza007@gmail.com>
2024-07-11 13:39:51 -0600

    Crear Solicitudes

---
+++ UMA-SYSTEM/UMA-SYSTEM.Backend/Controllers/SolicitudesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UMA_SYSTEM.Backend.Data;
+using UMA_SYSTEM.Backend.Models;
+
+namespace UMA_SYSTEM.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitudesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SolicitudesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Solicitudes
+                .Include(s => s.Usuario)
+                .ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Solicitud solicitud)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(e => e.Id == solicitud.IdUsuario);           
+
+            if (usuario != null)
+            {
+                solicitud.Usuario = usuario;               
+                _context.Add(solicitud);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Solicitud no puede ser nulo");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var solicitud = await _context.Solicitudes
+                .Include(x => x.Usuario)                
+                .SingleOrDefaultAsync(c => c.Id == id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+            return Ok(solicitud);
+        }
+
+    }
+}
---
+++ UMA-SYSTEM/UMA-SYSTEM.Frontend/Controllers/SolicitudesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UMA_SYSTEM.Frontend.Models;
+
+namespace UMA_SYSTEM.Frontend.Controllers
+{
+    public class SolicitudesController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+
+        public SolicitudesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7269/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/api/Solicitudes");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var solicitudes = JsonConvert.DeserializeObject<IEnumerable<Solicitud>>(content);
+                return View("Index", solicitudes);
+            }
+
+            return View(new List<Solicitud>());
+        }
+
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Solicitud solicitud)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Uri.EscapeDataString(User.Identity!.Name!);
+                var userResponse = await _httpClient.GetAsync($"/api/Usuarios/email/{email}");
+                var usuarioJson = await userResponse.Content.ReadAsStringAsync();
+                var user = JsonConvert.DeserializeObject<Usuario>(usuarioJson);
+                solicitud.Usuario = user!;
+                solicitud.IdUsuario = user!.Id;
+                solicitud.FechaSolicitud = DateTime.Now;
+                solicitud.FechaVencimiento = DateTime.Now.AddDays(3);
+                solicitud.FechaAprobacion = DateTime.Now.AddDays(3);
+                var json = JsonConvert.SerializeObject(solicitud);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync("/api/Solicitudes/", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["AlertMessage"] = "Solicitud creada exitosamente!!!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Error al crear la soliictud.");
+                    TempData["ErrorMessage"] = "Ocurrió un error al intentar crear la soliictud!!!";
+                }
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                TempData["ModelErrors"] = string.Join("\n", errors);
+            }
+            
+            return View(solicitud);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var response = await _httpClient.GetAsync($"/api/Solicitudes/{id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["ErrorMessage"] = "Error al obtener la solicitud.";
+                return RedirectToAction("Index");
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var solicitud = JsonConvert.DeserializeObject<Solicitud>(jsonString);
+
+            return View(solicitud);
+        }
+    }
+}
